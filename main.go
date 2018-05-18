8004f03fc7806f93c2987a4a2216dd442fdf4b91
adron <adronhall@gmail.com>
2018-05-18 15:51:06 -0700

    Added cobra defaults for doc defaults w/ Thrashing Code Team & respective email address.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2018 Thrashing Code Team <thrashingcode@thrashingcode.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
