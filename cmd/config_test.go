bec14c7344bcb2ab87444087cf927cf823a0e748
adron <adronhall@gmail.com>
2018-06-28 19:38:57 -0700

    Synching up with the latest before branch.

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -1,1 +1,32 @@
 package cmd
+
+import (
+	"testing"
+
+	"github.com/rendon/testcli"
+)
+
+func TestGreetings(t *testing.T) {
+	// Using package functions
+	testcli.Run("greetings")
+	if !testcli.Success() {
+		t.Fatalf("Expected to succeed, but failed: %s", testcli.Error())
+	}
+
+	if !testcli.StdoutContains("Hello?") {
+		t.Fatalf("Expected %q to contain %q", testcli.Stdout(), "Hello?")
+	}
+}
+
+func TestGreetingsWithName(t *testing.T) {
+	// Using the struct version, if you want to test multiple commands
+	c := testcli.Command("greetings", "--name", "John")
+	c.Run()
+	if !c.Success() {
+		t.Fatalf("Expected to succeed, but failed with error: %s", c.Error())
+	}
+
+	if !c.StdoutContains("Hello John!") {
+		t.Fatalf("Expected %q to contain %q", c.Stdout(), "Hello John!")
+	}
+}
