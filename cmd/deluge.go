3c88b029c64092450f7022c14eb9238acb2095c6
Adron Hall <adron@thrashingcode.com>
2018-08-17 17:05:45 -0700

    Testing the libraries.

--- cmd/deluge.go
+++ cmd/deluge.go
@@ -16,8 +16,11 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/Pallinder/go-randomdata"
 	"github.com/spf13/cobra"
+	"time"
+	"github.com/icrowley/fake"
+	"github.com/brianvoe/gofakeit"
 )
 
 // delugeCmd represents the deluge command
@@ -31,8 +34,71 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deluge called")
+		fmt.Println("Starting.")
+
+		repeats := 1000000
+
+		randLibStart := time.Now()
+		fmt.Println(randLibStart.Format(time.Stamp))
+		for i := 0; i < repeats; i++ {
+			randomdataLib()
+		}
+		randLibend := time.Now()
+		randLibelapsed := randLibend.Sub(randLibStart)
+		fmt.Printf("End randomdata Time: %s, %d elapsed.\n", randLibend.Format(time.Stamp), randLibelapsed.Nanoseconds())
+
+		fakeLibStart := time.Now()
+		fmt.Println(fakeLibStart.Format(time.Stamp))
+		for i := 0; i < repeats; i++ {
+			fakeLib()
+		}
+		fakeLibend := time.Now()
+		fakeLibelapsed := fakeLibend.Sub(fakeLibStart)
+		fmt.Printf("End fakelib Time: %s, %d elapsed.\n", fakeLibend.Format(time.Stamp), fakeLibelapsed.Nanoseconds())
+
+		gofakeitStart := time.Now()
+		fmt.Println(gofakeitStart.Format(time.Stamp))
+		for i := 0; i < repeats; i++ {
+			gofakeitLib()
+		}
+		gofakeitend := time.Now()
+		gofakeitelapsed := gofakeitend.Sub(gofakeitStart)
+		fmt.Printf("End gofakeite Time: %s, %d elapsed.\n", gofakeitend.Format(time.Stamp), gofakeitelapsed.Nanoseconds())
+
+
 	},
+}
+
+func gofakeitLib() {
+	gofakeit.Name()
+	gofakeit.Email()
+	gofakeit.Country()
+	gofakeit.City()
+	gofakeit.State()
+	gofakeit.Street()
+	gofakeit.StreetPrefix()
+	gofakeit.StreetName()
+	gofakeit.StreetNumber()
+}
+
+func fakeLib() {
+	fake.FullName()
+	fake.EmailAddress()
+	fake.Country()
+	fake.City()
+	fake.State()
+	fake.Street()
+	fake.StreetAddress()
+}
+
+func randomdataLib() {
+	randomdata.FullName(randomdata.RandomGender)
+	randomdata.Email()
+	randomdata.Country(randomdata.FullCountry)
+	randomdata.City()
+	randomdata.State(randomdata.Large)
+	randomdata.Street()
+	randomdata.Address()
 }
 
 func init() {
