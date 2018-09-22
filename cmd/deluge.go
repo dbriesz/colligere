9d1adc159d5f128a3b07f5197ab73162e3e5da35
Adron Hall <adron@thrashingcode.com>
2018-09-21 18:34:26 -0700

    Latest Twitch. Working with gocql.

--- cmd/deluge.go
+++ cmd/deluge.go
@@ -15,12 +15,7 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/Pallinder/go-randomdata"
 	"github.com/spf13/cobra"
-	"time"
-	"github.com/icrowley/fake"
-	"github.com/brianvoe/gofakeit"
 )
 
 // delugeCmd represents the deluge command
@@ -33,84 +28,9 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Starting.")
-
-		repeats := 1000000
-
-		randLibStart := time.Now()
-		fmt.Println(randLibStart.Format(time.Stamp))
-		for i := 0; i < repeats; i++ {
-			randomdataLib()
-		}
-		randLibend := time.Now()
-		randLibelapsed := randLibend.Sub(randLibStart)
-		fmt.Printf("End randomdata Time: %s, %d elapsed.\n", randLibend.Format(time.Stamp), randLibelapsed.Nanoseconds())
-
-		fakeLibStart := time.Now()
-		fmt.Println(fakeLibStart.Format(time.Stamp))
-		for i := 0; i < repeats; i++ {
-			fakeLib()
-		}
-		fakeLibend := time.Now()
-		fakeLibelapsed := fakeLibend.Sub(fakeLibStart)
-		fmt.Printf("End fakelib Time: %s, %d elapsed.\n", fakeLibend.Format(time.Stamp), fakeLibelapsed.Nanoseconds())
-
-		gofakeitStart := time.Now()
-		fmt.Println(gofakeitStart.Format(time.Stamp))
-		for i := 0; i < repeats; i++ {
-			gofakeitLib()
-		}
-		gofakeitend := time.Now()
-		gofakeitelapsed := gofakeitend.Sub(gofakeitStart)
-		fmt.Printf("End gofakeite Time: %s, %d elapsed.\n", gofakeitend.Format(time.Stamp), gofakeitelapsed.Nanoseconds())
-
-
-	},
-}
-
-func gofakeitLib() {
-	gofakeit.Name()
-	gofakeit.Email()
-	gofakeit.Country()
-	gofakeit.City()
-	gofakeit.State()
-	gofakeit.Street()
-	gofakeit.StreetPrefix()
-	gofakeit.StreetName()
-	gofakeit.StreetNumber()
-}
-
-func fakeLib() {
-	fake.FullName()
-	fake.EmailAddress()
-	fake.Country()
-	fake.City()
-	fake.State()
-	fake.Street()
-	fake.StreetAddress()
-}
-
-func randomdataLib() {
-	randomdata.FullName(randomdata.RandomGender)
-	randomdata.Email()
-	randomdata.Country(randomdata.FullCountry)
-	randomdata.City()
-	randomdata.State(randomdata.Large)
-	randomdata.Street()
-	randomdata.Address()
 }
 
 func init() {
 	rootCmd.AddCommand(delugeCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// delugeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// delugeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
